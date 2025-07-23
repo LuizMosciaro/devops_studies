63f1c120756061caa2fa7c00d0c50b656e3971e0
LuizMosciaro <edu.mosciaro@live.com>
2025-07-23 17:14:52 -0300

    starting phase 2 with flask crud

--- src/phase1/backup_and_sync.py
+++ src/phase1/backup_and_sync.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+from time import sleep
 import os, uuid
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
@@ -20,12 +21,23 @@ Build a small CLI interface with argparse,
 
 Add notification via Slack or Telegram for success/error."""
 
+FILENAME = 'sysbackup.tar.gz'
+
+account_url = "https://devopsstudyphase12025.blob.core.windows.net"
+default_credential = DefaultAzureCredential()
+
+# Create the BlobServiceClient object
+blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+
+# Create a blob client using the local file name as the name for the blob
+blob_client = blob_service_client.get_container_client(container='container-teste-blob')
+
 
 def compress_critical_directories() -> None:
     """
     Creates a compressed backup of critical directories using tar and gzip,
     then generates a SHA256 checksum file for the resulting archive.
-    The backup is saved as 'sysbackup.tar.gz' and the checksum as 'sysbackup.tar.gz.sha256'.
+    The backup is saved as FILENAME and the checksum as FILENAMEsha256'.
     """
     subprocess.run(
         "sudo tar -czf sysbackup.tar.gz /bin /tmp /etc && sha256sum sysbackup.tar.gz > sysbackup.tar.gz.sha256",
@@ -34,29 +46,40 @@ def compress_critical_directories() -> None:
     )
 
 
-def send_sysbackup_azure() -> None:
+def upload_sysbackup_azure() -> None:
     try:
-        account_url = "https://devopsstudyphase12025.blob.core.windows.net"
-        default_credential = DefaultAzureCredential()
-
-        # Create the BlobServiceClient object
-        blob_service_client = BlobServiceClient(account_url, credential=default_credential)
-
-        # Create a blob client using the local file name as the name for the blob
-        blob_client = blob_service_client.get_container_client(container='container-teste-blob')
-
         # Upload the created file
-        with open(file='sysbackup.tar.gz', mode="rb") as data:
-            blob_client.upload_blob(name='sysbackup.tar.gz',data=data)
-        print('File "sysbackup.tar.gz" sent to Azure')
-
-        blob_list = blob_client.list_blobs()
-        for blob in blob_list:
-            print("\t" + blob.name)
-
+        with open(file=FILENAME, mode="rb") as data:
+            blob_client.upload_blob(name=FILENAME,data=data)
+        print(f'File {FILENAME} sent to Azure')
     except Exception as ex:
         print('Exception:')
         print(ex)
 
+def check_file_integrity() -> None:
+    print("Checking backup file integrity")
+    subprocess.run(
+        "sudo sha256sum -c sysbackup.tar.gz.sha256",
+        shell=True,
+        check=True
+    )
+    
+def download_sysbackup_azure() -> None:
+    try:
+        # Download the created file
+        blob_client.download_blob(FILENAME)
+        print(f'File {FILENAME} downloaded from Azure')
+        with open(file=FILENAME, mode="wb") as sample_blob:
+            download_stream = blob_client.download_blob(FILENAME)
+            sample_blob.write(download_stream.readall())
+    except Exception as ex:
+        print('Exception:')
+        print(ex)
+    finally:
+        check_file_integrity()
+
+
 if __name__ == "__main__":
-    send_sysbackup_azure()
+    compress_critical_directories()
+    upload_sysbackup_azure()
+    download_sysbackup_azure()
